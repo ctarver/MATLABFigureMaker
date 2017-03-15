749bcb7cbea13eaccf9b6061d2dc5034a55f47bb
Chance <tarver.chance@gmail.com>
2017-03-15 09:24:29 -0500

    Give variable names to things

--- main.m
+++ main.m
@@ -1,4 +1,7 @@
-set(gcf, 'PaperPosition', [0 0 6 4]); %Position the plot further to the left and down. Extend the plot to fill entire paper.
-set(gcf, 'PaperSize', [6 4]); %Keep the same paper size
+WIDTH  = 6;
+HEIGHT = 4;
+
+set(gcf, 'PaperPosition', [0 0 WIDTH HEIGHT]); %Position the plot further to the left and down. Extend the plot to fill entire paper.
+set(gcf, 'PaperSize', [WIDTH HEIGHT]); %Keep the same paper size
 saveas(gcf, 'filename', 'pdf')
 open('filename.pdf');
